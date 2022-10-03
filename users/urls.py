ff3eddfc6e8dfe9faa165e629279a11f388d8549
indranillingayat <indranil3185@gmail.com>
2022-10-03 23:51:44 +0530

    Adding test login view

---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from users.views import CustomObtainAuthToken
+
+urlpatterns = [
+    path('login/', CustomObtainAuthToken.as_view())
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
 
 # Create your views here.
+class CustomObtainAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email', '@')
+        password = request.data.get('password', '')
+        username = email.split('@')[0]
+        serializer = self.get_serializer(data={'username': username, 'password': password})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+

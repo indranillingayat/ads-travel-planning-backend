a057a8e58f30dbe8964a06019614071931decde7
indranillingayat <indranil3185@gmail.com>
2022-10-06 23:06:23 +0530

    Correcting health check issues

--- ads_travel_planning/middleware.py
+++ ads_travel_planning/middleware.py
@@ -8,10 +8,10 @@ class AppMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.method == "OPTIONS" and "HTTP_ACCESS_CONTROL_REQUEST_METHOD" in request.META:
-            response = HttpResponse()
-            response["Content-Length"] = "0"
-            return self._add_response_headers(response)
+        # In case of pre-flight requests or health check request - send empty successful responses
+        if (request.method == "OPTIONS" and "HTTP_ACCESS_CONTROL_REQUEST_METHOD" in request.META) or \
+                request.path == '/health-check':
+            return self._send_empty_success_response()
         response = self.get_response(request)
         return self._add_response_headers(response)
 
@@ -23,14 +23,15 @@ class AppMiddleware:
         response["Access-Control-Expose-Headers"] = "*"
         return response
 
+    @staticmethod
+    def _send_empty_success_response():
+        response = HttpResponse()
+        response["Content-Length"] = "0"
+        return AppMiddleware._add_response_headers(response)
+
         # ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin"
         # ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers"
         # ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials"
         # ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers"
         # ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods"
         # ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age"
-        return response
-
-    def process_request(self, request: HttpRequest) -> HttpResponse | None:
-
-        return None

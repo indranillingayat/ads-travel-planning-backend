68b01c86ee44db41e42ca4744bcd8acd0008c6ad
indranillingayat <indranil3185@gmail.com>
2022-10-06 20:23:52 +0530

    Adding new allowed host

--- ads_travel_planning/app_settings/staging.py
+++ ads_travel_planning/app_settings/staging.py
@@ -5,4 +5,5 @@ from utils.singleton import Singleton
 class AppSettings(Singleton, BaseSettings):
     db_host = 'ads-test-database.c13lsk8zdesp.us-east-1.rds.amazonaws.com'
     db_port = 5432
-    allowed_hosts = ['adstravelplannerstaging-env.eba-fpbsp8bs.us-east-1.elasticbeanstalk.com']
+    allowed_hosts = ['adstravelplannerstaging-env.eba-fpbsp8bs.us-east-1.elasticbeanstalk.com',
+                     'staging-travel-planner.adsba-test.com']

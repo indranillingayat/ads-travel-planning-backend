774bac380eba55831c3c22cfcf52224df8089086
indranillingayat <indranil3185@gmail.com>
2022-10-28 21:18:15 +0530

    Adding trip visits

--- trips/views.py
+++ trips/views.py
@@ -1,9 +1,9 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
-from trips.models import Trip
+from trips.models import Trip, TripVisit
 from trips.permissions import TripPermission
-from trips.serializers import TripSerializer
+from trips.serializers import TripSerializer, TripVisitSerializer
 from utils.views.viewset import AppViewProvider
 
 
@@ -13,4 +13,10 @@ class TripViewSet(AppViewProvider, ModelViewSet):
     queryset = Trip.objects.all()
 
     def perform_create(self, serializer):
-        instance = serializer.save(owner=self.request.user)  # type: Trip
+        serializer.save(owner=self.request.user)
+
+
+class TripVisitViewSet(AppViewProvider, ModelViewSet):
+    serializer_class = TripVisitSerializer
+    queryset = TripVisit.objects.all()
+    filter_fields = ('trip', 'visit_account')
--- users/utils.py
+++ users/utils.py
@@ -1,7 +1,7 @@
 def get_allowed_user_actions(user):
     return [
         {'title': 'My visits manager', 'path': '/', 'icon': 'tour'},
-        {'title': 'Add new trip', 'path': '/trip/new', 'icon': 'add'},
-        {'title': 'Shared trips', 'path': '/trip/shared', 'icon': 'share'},
-        {'title': 'Trip requests', 'path': '/trip/requests', 'icon': 'flight'},
+        {'title': 'Add new trip', 'path': '/trips/new', 'icon': 'add'},
+        {'title': 'Shared trips', 'path': '/trips/shared', 'icon': 'share'},
+        {'title': 'Trip requests', 'path': '/trips/requests', 'icon': 'flight'},
     ]

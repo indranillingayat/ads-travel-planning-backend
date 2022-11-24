7c88e97f5ac56ae90338532ebe8051e324550af9
indranillingayat <indranil3185@gmail.com>
2022-11-24 21:49:40 +0530

    Create trip api updates

--- trips/serializers.py
+++ trips/serializers.py
@@ -10,6 +10,7 @@ class TripSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trip
         fields = '__all__'
+        read_only_fields = ('id', 'owner')
 
 
 class TripVisitSerializer(serializers.ModelSerializer):
@@ -17,3 +18,30 @@ class TripVisitSerializer(serializers.ModelSerializer):
     class Meta:
         model = TripVisit
         fields = '__all__'
+        read_only_fields = ('id', 'account_snapshot')
+
+
+class EmbeddedTripVisitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TripVisit
+        fields = ('id', 'note', 'visit_date', 'share_with_ads', 'share_externally', 'visit_account', 'start_time',
+                  'end_time')
+        read_only_fields = ('id',)
+
+
+class TripWithVisitsSerializer(serializers.ModelSerializer):
+    owner = UserMinInfoSerializer(read_only=True)
+    tripvisit_set = EmbeddedTripVisitSerializer(many=True)
+
+    def create(self, validated_data):
+        trip_visits = validated_data.pop('tripvisit_set')
+        trip = Trip.objects.create(**validated_data)
+        for visit in trip_visits:
+            TripVisit.objects.create(trip=trip, **visit)
+
+        return trip
+
+    class Meta:
+        model = Trip
+        fields = ('id', 'trip_name', 'comments', 'start_date', 'end_date', 'owner', 'tripvisit_set')
+        read_only_fields = ('id', 'owner')
--- trips/views.py
+++ trips/views.py
@@ -1,16 +1,17 @@
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from trips.models import Trip, TripVisit
 from trips.permissions import TripPermission
-from trips.serializers import TripSerializer, TripVisitSerializer
+from trips.serializers import TripSerializer, TripVisitSerializer, TripWithVisitsSerializer
 from utils.views.viewset import AppViewProvider
 
 
 class TripViewSet(AppViewProvider, ModelViewSet):
     permission_classes = [IsAuthenticated, TripPermission, ]
-    serializer_class = TripSerializer
-    queryset = Trip.objects.all()
+    serializer_class = TripWithVisitsSerializer
+    queryset = Trip.objects.none()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
